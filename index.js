d57781ba59b03d79a6742340427989cd28e48ea3
Simon Guest <simon.guest@concur.com>
2015-04-09 14:24:49 -0700

    updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World 3!');
+    res.send('Env Variable: '+process.env.SIMON);
 });
 
 var server = app.listen(3000, function () {
