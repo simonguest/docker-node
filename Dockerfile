7dd3d1309cbb51173ea902fb5e22f802b32e527a
Simon Guest <simon.guest@concur.com>
2015-04-10 09:25:28 -0700

    Added phantom get

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,16 @@ FROM dockerfile/nodejs
 # Version
 RUN echo 1.0.3
 
+# apt-get update
+RUN apt-get update
+
+# Install PhantomJS
+RUN apt-get install bzip2 curl libfreetype6 libfontconfig1  -y
+RUN curl -k -O https://phantomjs.googlecode.com/files/phantomjs-1.9.2-linux-x86_64.tar.bz2
+RUN tar -xvf phantomjs-1.9.2-linux-x86_64.tar.bz2 && rm phantomjs-1.9.2-linux-x86_64.tar.bz2
+RUN mv phantomjs-1.9.2-linux-x86_64 /usr/local/phantomjs-1.9.2-linux-x86_64
+RUN ln -s /usr/local/phantomjs-1.9.2-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs
+
 # Pull latest code
 RUN mkdir /var/site
 WORKDIR /var/site
--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 var express = require('express');
 var app = express();
+var phantom = require('phantom');
+
+app.get('/pdf', function (req, res) {
+    console.log(req.query.host);
+    console.log(req.query.port);
+    phantom.create(function (ph) {
+        ph.createPage(function (page) {
+            page.open('http://'+req.query.host+':'+req.query.port, function (status) {
+                console.log(status);
+                page.set('paperSize', {
+                    format: 'Legal',
+                    orientation: 'landscape'
+                }, function () {
+                    page.render(__dirname + '/tmp/report.pdf', function () {
+                        res.sendFile(__dirname + '/tmp/report.pdf');
+                        ph.exit();
+                    });
+                });
+            });
+        });
+    });
+});
 
 app.get('/', function (req, res) {
-    res.send('Environment Variable 4: '+process.env.SIMON);
+    res.send('Hello ' + process.env.SIMON);
 });
 
 var server = app.listen(3000, function () {
