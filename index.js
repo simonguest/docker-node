e46977ce8ffbaaebc84ed11cc3e16c005b565c4b
Simon Guest <simon.guest@concur.com>
2015-04-15 16:54:52 -0700

    Tidy up commit

--- index.js
+++ index.js
@@ -1,37 +1,12 @@
 var express = require('express');
 var app = express();
-var phantom = require('phantom');
-
-app.get('/pdf', function (req, res) {
-    console.log(req.query.host);
-    console.log(req.query.port);
-    phantom.create(function (ph) {
-        ph.createPage(function (page) {
-            page.open('http://'+req.query.host+':'+req.query.port, function (status) {
-                console.log(status);
-                page.set('paperSize', {
-                    format: 'Legal',
-                    orientation: 'landscape'
-                }, function () {
-                    page.render(__dirname + '/tmp/report.pdf', function () {
-                        res.sendFile(__dirname + '/tmp/report.pdf');
-                        ph.exit();
-                    });
-                });
-            });
-        });
-    });
-});
 
 app.get('/', function (req, res) {
-    res.send('<p style="font-family:Ubuntu Condensed">Hello World 3 - value is: ' + process.env.SIMON + '</p>');
+    res.send('<p id="hello">Hello World!</p>');
 });
 
 var server = app.listen(3000, function () {
-
     var host = server.address().address;
     var port = server.address().port;
-
-    console.log('Example app listening at http://%s:%s', host, port);
-
+    console.log('Example app listening on port: ', port);
 });
