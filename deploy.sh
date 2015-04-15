d5edb2f6c3164e6019e6b6a8a126cb98d660e147
Simon Guest <simon.guest@concur.com>
2015-04-15 16:26:53 -0700

    Added AWS deployment

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,14 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
 SHA1=$1
 
-# Deploy image to Docker Hub
-docker push simonguest/docker-node:$SHA1
+# Copy docker json to S3
+DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
+sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
+aws s3 cp $DOCKERRUN_FILE s3://circleci-builds/$DOCKERRUN_FILE
 
-# Create new Elastic Beanstalk version
-#DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
-#sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
-#aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-#aws elasticbeanstalk create-application-version --application-name hello \
-#    --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
+# Create new application version
+aws elasticbeanstalk create-application-version --application-name concur-ui --version-label $SHA1 --source-bundle S3Bucket=circleci-builds,S3Key=$DOCKERRUN_FILE
 
-# Update Elastic Beanstalk environment to new version
-#aws elasticbeanstalk update-environment --environment-name hello-env --version-label $SHA1
+# Update environement to the new version
+aws elasticbeanstalk update-environment --environment-name concur-ui-qa --version-label $SHA1
