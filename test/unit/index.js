90d3629370d831258339ccf13f7e05d9d674172b
Simon Guest <simon.guest@concur.com>
2015-04-15 22:18:00 -0700

    Adding webdriver/phantomjs tests

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,7 +1,7 @@
 var assert = require('chai').assert;
-var data = require('../message');
+var data = require('../../message');
 
-describe('First test', function(){
+describe('Unit tests', function(){
     it('should return Hello World', function(done){
         var result = data.getMessage();
         assert(result === 'Hello World', 'Data mismatch');
---
+++ test/webdriver/index.js
@@ -0,0 +1,20 @@
+var assert = require('chai').assert;
+var webdriver = require('selenium-webdriver'),
+    By = require('selenium-webdriver').By;
+
+describe('Webdriver tests', function () {
+    this.timeout(5000);
+    it('should return the mock event', function (done) {
+        var driver = new webdriver.Builder()
+            .forBrowser('phantomjs')
+            .build();
+        driver.get('http://localhost:3000');
+        driver.findElement(By.id('hello')).getText().then(function (text) {
+            assert(text === 'Hello World', 'Expected page to display Hello World');
+            done();
+        });
+        driver.quit();
+    });
+});
+
+
