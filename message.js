e1bd230446c0b6787507d2cb2e3eed4cc102194f
Simon Guest <simon.guest@concur.com>
2015-04-22 08:13:33 -0700

    Updated demo

--- message.js
+++ message.js
@@ -1,4 +1,4 @@
 'use strict';
 module.exports.getMessage = function(){
-    return 'Hello Radmin';
+    return 'Hello World 2';
 };
--- test/unit/index.js
+++ test/unit/index.js
@@ -4,7 +4,7 @@ var data = require('../../message');
 describe('Unit tests', function(){
     it('should return Hello World', function(done){
         var result = data.getMessage();
-        assert(result === 'Hello Radim', 'Data mismatch');
+        assert(result === 'Hello World 2', 'Data mismatch');
         done();
     });
 });
